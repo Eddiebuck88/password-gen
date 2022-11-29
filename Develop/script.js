0d24a7c067c2e3ce1d0da0363e5bb15addd5cbbb
edmond buck <edmond.buck@wearetissue.com>
2022-11-29 01:15:19 -0800

    made functions

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,16 @@ var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","[","{","]","}","|",":",";","'","/","?",".",">",",","<"];
 
+// empty array to pass options
+var compOptions = [];
 
+// empty string to pass option to make your password
+var madePassword ="";
+
+function chooseRandomCharacter (min, max) {
+  var randomSelection = Math.random()
+  return Math.floor(min + (1 - randomSelection))
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
